02a0b5bf50712a90eaea707b5762a4af54eee04b
Jakob Lykke Andersen <Jakob@caput.dk>
2025-05-21 09:07:26 +0200

    update copyright

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -13,7 +13,7 @@ extensions = [#'sphinx.ext.intersphinx',
 cpp_index_common_prefix = ["graph_canon::"] 
 templates_path = ['_templates']
 project = u'GraphCanon'
-copyright = u'2024, Jakob Lykke Andersen'
+copyright = u'2017-2025, Jakob Lykke Andersen'
 author = u'Jakob Lykke Andersen'
 
 with open("../../VERSION") as f:
